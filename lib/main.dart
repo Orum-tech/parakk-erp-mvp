6563bed57cbaf2baebc4f6044a7a55c3a3af3f4c
Amritesh Pandey <amriteshpandey@Amriteshs-MacBook-Air.local>
2026-01-09 12:57:39 +0530

    Auth completed login signup

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 // FILE: lib/main.dart
 import 'package:flutter/material.dart';
-import 'screens/role_selection_screen.dart'; // First screen import ki
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'screens/auth_wrapper.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const ParakkApp());
 }
 
@@ -15,17 +21,16 @@ class ParakkApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Parakk ERP',
       theme: ThemeData(
-        // "Decent Bluish" Global Theme setup
-        primaryColor: const Color(0xFF1565C0), 
+        primaryColor: const Color(0xFF1565C0),
         scaffoldBackgroundColor: Colors.white,
         fontFamily: 'Roboto',
         useMaterial3: true,
         colorScheme: ColorScheme.fromSeed(
-          seedColor: const Color(0xFF1565C0), // Royal Blue base
+          seedColor: const Color(0xFF1565C0),
           brightness: Brightness.light,
         ),
       ),
-      home: const RoleSelectionScreen(), // App yahan se start hoga
+      home: const AuthWrapper(),
     );
   }
 }
